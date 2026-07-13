b339bada0d50578b85a95909f136f8cdc6842d73
sumit1103 <sumitchandra960@gmail.com>
2026-07-13 11:58:48 +0530

    Added program.py and pythonnotes.txt

---
+++ program.py
@@ -0,0 +1,22 @@
+print("Hello, World!")
+print("Hello!", end=" ")
+print("Hello, World!", end=" - ")
+print("Thank you")
+print("Good Morning! \nThank You")
+print("1")
+print("2")
+print("1", end="\n")
+a = "Hello"
+b = "worls"
+print(a + b)
+
+name = "Sumit"
+age = 23
+#print("My name is " + name + " and age is age " + age)
+print("My name is " + name + " and age is age " + str(age) + "!")
+print("My name is " + name + " and age is age ",age)
+
+#String formatting
+print(f"My name is {name} and age is {age}!") # f - is used to
+print("Name:\tSumit")
+print("Age:\n23")
